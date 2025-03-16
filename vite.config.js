35660027945e204359e06adc166361d16c8f2f41
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-03-16 21:29:52 +0500

    api update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,4 +13,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_APP_API, // API manzili
+        changeOrigin: true,
+        secure: true, // HTTPS bo‘lsa true, HTTP bo‘lsa false
+      }
+    }
+  }
 })
