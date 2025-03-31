4c5fee54b5b0c4c28055668422a01d24c3a18332
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-03-31 23:41:58 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,27 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_APP_API || 'http://localhost:5000', // .env fayldan URL ni oladi
-        changeOrigin: true,
-        secure: true, // HTTPS bo‘lsa true, HTTP bo‘lsa false
-        rewrite: (path) => path.replace(/^\/api/, ''), // `/api` ni o‘chirib tashlaydi
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()); // .env faylini yuklaydi
+
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_API || 'http://localhost:5000', // `env` dan olish
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
 });
