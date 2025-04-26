6594cb62f98989e857e8ea023a8d9f70a8d75fba
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-26 16:42:54 +0500

    update

--- src/views/Commerce-cars/CommerceCars.vue
+++ src/views/Commerce-cars/CommerceCars.vue
@@ -12,20 +12,20 @@
     <!-- ===== filter ===== -->
     <h1 class="main-header mt-3">{{ $t('pages.CommercialTransport.filter.header') }}</h1>
     <Filter />
-    <div v-if="loading" class="loader shadow">
+    <div v-if="loading || loadingFilter" class="loader shadow">
       <a-spin />
     </div>
     <!-- ===== Commerce cars ===== -->
     <AvtoKatalok v-else-if="commerceCars.length" :avtomobiles="commerceCars" :new-current="current"
       router="/commerce-cars/commerce-cars-tovar" />
-    <a-result v-else-if="error" status="404" title="404" sub-title="Sorry, An error occurred while loading the data!" />
+    <a-result v-else-if="error || errorFilter" status="404" title="404" sub-title="Sorry, An error occurred while loading the data!" />
     <!-- ===== Request ===== -->
     <ResponseBanner />
   </div>
 </template>
 <script setup>
 //vue
-import { ref, watch } from 'vue';
+import { ref, watch, watchEffect } from 'vue';
 import { RouterLink } from 'vue-router';
 //components
 import Filter from '@/components/Filter/Filter.vue';
@@ -33,6 +33,12 @@ import AvtoKatalok from '@/components/AvtoKatalok/AvtoKatalok.vue';
 import { ResponseBanner } from '@/components';
 //Hooks
 import { useFetch } from '@/Hooks/UseFatch';
+//filter Hook
+import { useFilteredCars } from '@/Hooks/useFilteredCars';
+//filter Store
+import { useFilterStore } from '@/Stores/useFilter';
+
+const filters = useFilterStore()
 
 const commerceCars = ref([]);
 const current = ref(1);
@@ -41,8 +47,33 @@ const { data, loading, error } = useFetch(
   `${import.meta.env.VITE_APP_API}/commerce-cars`
 );
 
+const { carsFilter, loadingFilter, errorFilter, fetchFilteredCars } = useFilteredCars(`${import.meta.env.VITE_APP_API}/commerce-filter`)
+
 watch(data, (newData) => {
-  commerceCars.value = newData;
+  if (newData) {
+    commerceCars.value = newData;
+  }
+});
+
+watch(
+  () => filters.stock,
+  async (newFilters) => {
+    const hasFilter = Object.values(newFilters).some(
+      (val) => val !== null && val !== '' && !(Array.isArray(val) && val.length === 0)
+    );
+    if (hasFilter) {
+      await fetchFilteredCars(newFilters);
+    }
+  },
+  { deep: true }
+);
+
+watchEffect(() => {
+  if (carsFilter.value.length > 0) {
+    commerceCars.value = carsFilter.value;
+  } else if (filters.stock) {
+    commerceCars.value = [];
+  }
 });
 </script>
 <style></style>
