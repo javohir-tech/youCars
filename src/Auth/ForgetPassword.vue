333b24c12ab0a1e3aa8d45539e882cab300a2583
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-04 15:43:39 +0500

    forgetpassword is update

--- src/Auth/ForgetPassword.vue
+++ src/Auth/ForgetPassword.vue
@@ -1,41 +1,28 @@
 <template>
   <div class="registration-container">
     <div class="registration-card">
-      <h1 class="title">Забыли пароль?</h1>
-      <p class="subtitle">Введите e-mail для восстановления</p>
+      <h1 class="title">{{ $t('auth.forgetPassword.header') }}</h1>
+      <p class="subtitle">{{ $t('auth.forgetPassword.title') }}</p>
       <div>
-        <a-form
-          :model="formState"
-          name="basic"
-          autocomplete="off"
-          @finish="onFinish"
-          @finishFailed="onFinishFailed"
-        >
-          <a-form-item
-            name="email"
-            :rules="[
-              {
-                required: true,
-                message: 'Please input your email!',
-                type: 'email',
-              },
-            ]"
-          >
+        <a-form :model="formState" name="basic" autocomplete="off" @finish="onFinish" @finishFailed="onFinishFailed">
+          <a-form-item name="email" :rules="[
+            {
+              required: true,
+              message: 'Please input your email!',
+              type: 'email',
+            },
+          ]">
             <a-input v-model:value="formState.email" placeholder="E-mail" />
           </a-form-item>
 
-          <a-button
-            type="primary"
-            style="width: 100%; margin-top: 16px"
-            html-type="submit"
-            :disabled="loading"
-          >
+          <a-button type="primary" style="width: 100%; margin-top: 16px" html-type="submit" :disabled="loading">
             <a-spin size="small" v-if="loading" />
-            {{ loading ? 'Laoding...' : 'Войти' }}
+            {{ loading ? 'Laoding...' : $t('auth.forgetPassword.button') }}
           </a-button>
         </a-form>
         <div class="under-link">
-          <p>Вспомниили пароль?<RouterLink to="/login"> Войти</RouterLink></p>
+          <p>{{ $t('auth.forgetPassword.subtitle') }}<RouterLink to="/login">{{ $t('navbar.auth.login') }}</RouterLink>
+          </p>
         </div>
       </div>
     </div>
@@ -46,7 +33,9 @@
 import { message } from 'ant-design-vue';
 import axios from 'axios';
 import { reactive, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 
+const t = useI18n()
 const loading = ref(false);
 
 const formState = reactive({
