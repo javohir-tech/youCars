f5df9867f7641365a99f5dd1805191ea5b9cd6f5
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-09 01:36:05 +0500

    update

--- clone/Easy/mySqrt.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} x
- * @return {number}
- */
-var mySqrt = function(x) {
-    return Math.pow(x, 0.5)
-};
-
-console.log(mySqrt(4))
--- clone/studyPlan/reduceRight.js
+++
@@ -1,8 +0,0 @@
-const arr = [1, 2, 3]
-
-const result = arr.reduceRight((acc, val)=>{
-    console.log(`acc : ${acc} , val : ${val}`)
-    return acc+val
-} , 0)
-
-console.log(result)
